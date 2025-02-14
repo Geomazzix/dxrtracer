952465f596a5e5ef8418472f2d72df26f18e18e7
Tom Lemmers <Geomazzix@gmail.com>
2025-02-14 16:44:32 +0000

    inline constexpr provides the compiler with more options to optimize.

--- dxray/core/include/core/vath/vathUtility.h
+++ dxray/core/include/core/vath/vathUtility.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "core/valueTypes.h"
 #include <random>
+#include <numbers>
 
 namespace dxray::vath
 {
@@ -25,85 +26,85 @@ namespace dxray::vath
 	//--- Arithmetic Utility ---
 
 	template<Arithmetic T>
-	constexpr T Clamp(const T a_value, const T a_min, const T a_max)
+	inline constexpr T Clamp(const T a_value, const T a_min, const T a_max)
 	{
 		return std::clamp<T>(a_value, a_min, a_max);
 	}
 
 	template <Arithmetic T>
-	constexpr T Min(const T a_value, const T a_min)
+	inline constexpr T Min(const T a_value, const T a_min)
 	{
         return std::min<T>(a_value, a_min);
 	}
 
 	template <Arithmetic T>
-	constexpr T Max(const T a_value, const T a_max)
+	inline constexpr T Max(const T a_value, const T a_max)
 	{
 		return std::max<T>(a_value, a_max);
 	}
 
 	template <Arithmetic T, Arithmetic S = T>
-	constexpr S Sign(const T a_value)
+	inline constexpr S Sign(const T a_value)
 	{
 		return std::signbit(a_value) ? static_cast<S>(-1) : static_cast<S>(1);
 	}
 
 	template <UnsignedArithmatic T>
-	constexpr T Abs(const T a_value)
+	inline constexpr T Abs(const T a_value)
 	{
 		return std::abs(a_value);
 	}
 
     template <FloatingPoint T>
-    constexpr T Floor(const T a_fp)
+	inline constexpr T Floor(const T a_fp)
     {
 		return std::floor(a_fp);
     }
 
     template <FloatingPoint T>
-    constexpr T Ceil(const T a_fp)
+	inline constexpr T Ceil(const T a_fp)
     {
 		return std::ceil(a_fp);
     }
 
 	template <FloatingPoint T>
-	constexpr T Round(const T a_fp)
+	inline constexpr T Round(const T a_fp)
 	{
 		return std::round(a_fp);
 	}
 
 	template <FloatingPoint T>
-	constexpr T Lerp(const T a_min, const T a_max, const T a_coefficient)
+	inline constexpr T Lerp(const T a_min, const T a_max, const T a_coefficient)
 	{
 		return std::lerp(a_min, a_max, a_coefficient);
 	}
 
 	template <FloatingPoint T>
-	constexpr T Infinity()
+	inline constexpr T Infinity()
 	{
 		return std::numeric_limits<T>::infinity();
 	}
 
 	template <FloatingPoint T>
-	constexpr T Epsilon()
+	inline constexpr T Epsilon()
 	{
 		return std::numeric_limits<T>::epsilon();
 	}
 
 	template<FloatingPoint T>
-	constexpr T Pi()
+	inline constexpr T Pi()
 	{
-		return (sizeof(T) > sizeof(float)) ? 3.141592653589793 : 3.1415926f;
+		return std::numbers::pi_v<fp32>;
 	}
 
 	template <FloatingPoint T>
-	constexpr T DegToRad(const T a_valueInDeg)
+	inline constexpr T DegToRad(const T a_valueInDeg)
 	{
 		return a_valueInDeg * (Pi<T>() / static_cast<T>(180));
 	}
 
 	template <FloatingPoint T>
-	constexpr T RadToDeg(const T a_valueInRad)
+	inline constexpr T RadToDeg(const T a_valueInRad)
 	{
 		return a_valueInRad * (static_cast<T>(180) / Pi<T>());
 	}
